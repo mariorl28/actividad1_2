41c1007f2d60c9a03e7f94c6d8ea23ad3630b3d9
Mario Rodriguez <mariorl28@educastur.es>
2023-09-22 19:26:10 +0200

    primera version

---
+++ src/main/java/pa/cifpaviles/dam/actividad1_2/SecondaryController.java
@@ -0,0 +1,12 @@
+package pa.cifpaviles.dam.actividad1_2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
